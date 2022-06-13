2f84e0a968807aaeb32b9e2941393df5b5e2fd9d
Douglas Lovell <doug@wbreeze.com>
2022-06-13 11:43:28 -0300

    Update ec2 image setup script with correct node install

--- script/ec2_image_setup.sh
+++ script/ec2_image_setup.sh
@@ -4,5 +4,5 @@ sudo yum install libm.so.6
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 . ~/.nvm/nvm.sh
-nvm install node 16
+nvm install --lts --latest-npm 14
 node -e "console.log('Running Node.js ' + process.version)"
