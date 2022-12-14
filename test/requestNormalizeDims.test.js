e3fed5dc2ace70a1cb9399ea3094c55084fe92f0
Douglas Lovell <doug@wbreeze.com>
2022-12-14 17:24:59 -0300

    Have normalized dims test use generic node test

--- test/requestNormalizeDims.test.js
+++ test/requestNormalizeDims.test.js
@@ -1,45 +1,48 @@
+'use strict';
+
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
 import { ImageRequestBuilder } from "../src/imageRequestBuilder.js";
 
-describe('Normalized dimension request', () => {
-  it('finds the smallest', () => {
-    let irb = ImageRequestBuilder({});
-    let dim = irb.allowedDimension({ w: 400, h: 400 })
-    expect(dim.w).toEqual(600);
-    expect(dim.h).toEqual(600);
-  });
+test('finds the smallest', () => {
+  let irb = ImageRequestBuilder({});
+  let dim = irb.allowedDimension({ w: 400, h: 400 })
+  assert.equal(dim.w, 600);
+  assert.equal(dim.h, 600);
+});
 
-  it('finds the largest', () => {
-    let irb = ImageRequestBuilder({});
-    let dim = irb.allowedDimension({ w: 3600, h: 3600 })
-    expect(dim.w).toEqual(3600);
-    expect(dim.h).toEqual(3600);
-  });
+test('finds the largest', () => {
+  let irb = ImageRequestBuilder({});
+  let dim = irb.allowedDimension({ w: 3600, h: 3600 })
+  assert.equal(dim.w, 3600);
+  assert.equal(dim.h, 3600);
+});
 
-  it('defaults the largest when request is larger', () => {
-    let irb = ImageRequestBuilder({});
-    let dim = irb.allowedDimension({ w: 36000, h: 36000 })
-    expect(dim.w).toEqual(3600);
-    expect(dim.h).toEqual(3600);
-  });
+test('defaults the largest when request is larger', () => {
+  let irb = ImageRequestBuilder({});
+  let dim = irb.allowedDimension({ w: 36000, h: 36000 })
+  assert.equal(dim.w, 3600);
+  assert.equal(dim.h, 3600);
+});
 
-  it('defaults the largest when none specified', () => {
-    let irb = ImageRequestBuilder({});
-    let dim = irb.allowedDimension(null)
-    expect(dim.w).toEqual(3600);
-    expect(dim.h).toEqual(3600);
-  });
+test('defaults the largest when none specified', () => {
+  let irb = ImageRequestBuilder({});
+  let dim = irb.allowedDimension(null)
+  assert.equal(dim.w, 3600);
+  assert.equal(dim.h, 3600);
+});
 
-  it('defaults the largest when garbage specified', () => {
-    let irb = ImageRequestBuilder({});
-    let dim = irb.allowedDimension({ martin: "martian" })
-    expect(dim.w).toEqual(3600);
-    expect(dim.h).toEqual(3600);
-  });
+test('defaults the largest when garbage specified', () => {
+  let irb = ImageRequestBuilder({});
+  let dim = irb.allowedDimension({ martin: "martian" })
+  assert.equal(dim.w, 3600);
+  assert.equal(dim.h, 3600);
+});
 
-  it('finds the 2400', () => {
-    let irb = ImageRequestBuilder({});
-    let dim = irb.allowedDimension({ w: 2400, h: 2400 })
-    expect(dim.w).toEqual(2400);
-    expect(dim.h).toEqual(2400);
-  });
+test('finds the 2400', () => {
+  let irb = ImageRequestBuilder({});
+  let dim = irb.allowedDimension({ w: 2400, h: 2400 })
+  assert.equal(dim.w, 2400);
+  assert.equal(dim.h, 2400);
 });
