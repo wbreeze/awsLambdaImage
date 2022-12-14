8fa03c1cafd5380177d9b6d0c343afc65cfe4c3a
Douglas Lovell <doug@wbreeze.com>
2022-12-14 17:34:21 -0300

    Have uri parse test use generic node test

--- test/requestURIParse.test.js
+++ test/requestURIParse.test.js
@@ -1,75 +1,74 @@
 'use strict';
 
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
 import { URIParser } from "../src/uriParser.js";
 
-describe('URIParser uri for prefix', () => {
-  it('returns prefix with numeric element last in path', () => {
-    let urip = URIParser('https://this/path/to/300/image.jpeg')
-    let prefix = urip.prefix();
-    expect(prefix).toEqual('https://this/path/to/300')
-  });
-
-  it('returns full prefix', () => {
-    let urip = URIParser('https://this/path/to/image.jpeg')
-    let prefix = urip.prefix();
-    expect(prefix).toEqual('https://this/path/to')
-  });
-
-  it('returns valid prefix if the path is short', () => {
-    let urip = URIParser('https://image.jpeg')
-    expect(urip.prefix()).toEqual('https:/')
-  });
+// URIParser uri for prefix
+test('returns prefix with numeric element last in path', () => {
+  let urip = URIParser('https://this/path/to/300/image.jpeg')
+  let prefix = urip.prefix();
+  assert.equal(prefix, 'https://this/path/to/300');
 });
 
-describe('URIParser uri for image name', () => {
-  it('returns image name if dimensions present', () => {
-    let urip = URIParser('https://this/path/to/300/image.jpeg')
-    let name = urip.imageName();
-    expect(name).toEqual('image');
-  });
-
-  it('returns image name if dimensions not present', () => {
-    let urip = URIParser('https://this/path/to/image.jpeg')
-    let name = urip.imageName();
-    expect(name).toEqual('image');
-  });
-
-  it('returns image name if dimensions not valid', () => {
-    let urip = URIParser('https://this/path/to/abcx300/image.jpeg')
-    let name = urip.imageName();
-    expect(name).toEqual('image');
-  });
+test('returns full prefix', () => {
+  let urip = URIParser('https://this/path/to/image.jpeg')
+  let prefix = urip.prefix();
+  assert.equal(prefix, 'https://this/path/to');
 });
 
-describe('URIParser uri for image extension', () => {
-  it('returns image ext if dimensions present', () => {
-    let urip = URIParser('https://this/path/to/300/image.jpeg')
-    let ext = urip.imageExtension();
-    expect(ext).toEqual('jpeg');
-  });
-
-  it('returns image ext if dimensions not present', () => {
-    let urip = URIParser('https://this/path/to/image.jpeg')
-    let ext = urip.imageExtension();
-    expect(ext).toEqual('jpeg');
-  });
-
-  it('returns image ext if dimensions not valid', () => {
-    let urip = URIParser('https://this/path/to/abcx300/image.jpeg')
-    let ext = urip.imageExtension();
-    expect(ext).toEqual('jpeg');
-  });
+test('returns valid prefix if the path is short', () => {
+  let urip = URIParser('https://image.jpeg')
+  assert.equal(urip.prefix(), 'https:/');
 });
 
-describe('URIParser Memoizes uri', () => {
-  it('returns memoized values', () => {
-    let urip = URIParser('https://this/is/the/way/to/800/grandma.png')
-    let ext = urip.imageExtension();
-    const memo = urip.elements;
-    expect(memo).toBeDefined();
-    expect(ext).toEqual('png');
-    expect(urip.imageName()).toEqual('grandma');
-    expect(urip.prefix()).toEqual('https://this/is/the/way/to/800');
-    expect(urip.elements).toBe(memo);
-  });
+// URIParser uri for image name
+test('returns image name if dimensions present', () => {
+  let urip = URIParser('https://this/path/to/300/image.jpeg')
+  let name = urip.imageName();
+  assert.equal(name, 'image');
+});
+
+test('returns image name if dimensions not present', () => {
+  let urip = URIParser('https://this/path/to/image.jpeg')
+  let name = urip.imageName();
+  assert.equal(name, 'image');
+});
+
+test('returns image name if dimensions not valid', () => {
+  let urip = URIParser('https://this/path/to/abcx300/image.jpeg')
+  let name = urip.imageName();
+  assert.equal(name, 'image');
+});
+
+// URIParser uri for image extension
+test('returns image ext if dimensions present', () => {
+  let urip = URIParser('https://this/path/to/300/image.jpeg')
+  let ext = urip.imageExtension();
+  assert.equal(ext, 'jpeg');
+});
+
+test('returns image ext if dimensions not present', () => {
+  let urip = URIParser('https://this/path/to/image.jpeg')
+  let ext = urip.imageExtension();
+  assert.equal(ext, 'jpeg');
+});
+
+test('returns image ext if dimensions not valid', () => {
+  let urip = URIParser('https://this/path/to/abcx300/image.jpeg')
+  let ext = urip.imageExtension();
+  assert.equal(ext, 'jpeg');
+});
+
+// URIParser Memoizes uri
+test('returns memoized values', () => {
+  let urip = URIParser('https://this/is/the/way/to/800/grandma.png')
+  let ext = urip.imageExtension();
+  const memo = urip.elements;
+  assert(memo);
+  assert.equal(ext, 'png');
+  assert.equal(urip.imageName(), 'grandma');
+  assert.equal(urip.prefix(), 'https://this/is/the/way/to/800');
+  assert.equal(urip.elements, memo);
 });
