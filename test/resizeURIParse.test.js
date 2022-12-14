80a01bd3f1991f1b2188cac690b5fe48e6d22287
Douglas Lovell <doug@wbreeze.com>
2022-12-14 17:40:19 -0300

    Have resize uri parse test use generic node test

--- test/resizeURIParse.test.js
+++ test/resizeURIParse.test.js
@@ -1,57 +1,58 @@
 'use strict';
 
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
 import { URIParser } from '../src/uriParser.js';
 
-describe('URIParser components', () => {
-  it('parses expected format', () => {
-    const testURI = 'https://path/to/300x400/jpeg/image.jpeg';
-    let urip = URIParser(testURI);
-    let parts = urip.getParts();
-    expect(parts.width).toEqual(300);
-    expect(parts.height).toEqual(400);
-    expect(parts.prefix).toEqual('path/to');
-    expect(parts.requiredFormat).toEqual('jpeg');
-    expect(parts.imageName).toEqual('image.jpeg');
-    expect(parts.sourceKey).toEqual('path/to/image.jpeg');
-    expect(parts.scaledKey).toEqual('path/to/300x400/jpeg/image.jpeg');
-  });
+test('parses expected format', () => {
+  const testURI = 'https://path/to/300x400/jpeg/image.jpeg';
+  let urip = URIParser(testURI);
+  let parts = urip.getParts();
+  assert.equal(parts.width, 300);
+  assert.equal(parts.height, 400);
+  assert.equal(parts.prefix, 'path/to');
+  assert.equal(parts.requiredFormat, 'jpeg');
+  assert.equal(parts.imageName, 'image.jpeg');
+  assert.equal(parts.sourceKey, 'path/to/image.jpeg');
+  assert.equal(parts.scaledKey, 'path/to/300x400/jpeg/image.jpeg');
+});
 
-  it('parses non ssl format', () => {
-    const testURI = 'http://path/to/300x400/jpeg/image.jpeg'
-    let urip = URIParser(testURI)
-    let parts = urip.getParts();
-    expect(parts.width).toEqual(300);
-    expect(parts.height).toEqual(400);
-    expect(parts.prefix).toEqual('path/to');
-    expect(parts.requiredFormat).toEqual('jpeg');
-    expect(parts.imageName).toEqual('image.jpeg');
-    expect(parts.sourceKey).toEqual('path/to/image.jpeg');
-    expect(parts.scaledKey).toEqual('path/to/300x400/jpeg/image.jpeg');
-  });
+test('parses non ssl format', () => {
+  const testURI = 'http://path/to/300x400/jpeg/image.jpeg'
+  let urip = URIParser(testURI)
+  let parts = urip.getParts();
+  assert.equal(parts.width, 300);
+  assert.equal(parts.height, 400);
+  assert.equal(parts.prefix, 'path/to');
+  assert.equal(parts.requiredFormat, 'jpeg');
+  assert.equal(parts.imageName, 'image.jpeg');
+  assert.equal(parts.sourceKey, 'path/to/image.jpeg');
+  assert.equal(parts.scaledKey, 'path/to/300x400/jpeg/image.jpeg');
+});
 
-  it('parses format without prefix', () => {
-    const testURI = '/300x400/jpeg/image.jpeg'
-    let urip = URIParser(testURI)
-    let parts = urip.getParts();
-    expect(parts.width).toEqual(300);
-    expect(parts.height).toEqual(400);
-    expect(parts.prefix).toBe("")
-    expect(parts.requiredFormat).toEqual('jpeg');
-    expect(parts.imageName).toEqual('image.jpeg');
-    expect(parts.sourceKey).toEqual('image.jpeg');
-    expect(parts.scaledKey).toEqual('300x400/jpeg/image.jpeg');
-  });
+test('parses format without prefix', () => {
+  const testURI = '/300x400/jpeg/image.jpeg'
+  let urip = URIParser(testURI)
+  let parts = urip.getParts();
+  assert.equal(parts.width, 300);
+  assert.equal(parts.height, 400);
+  assert.equal(parts.prefix, '');
+  assert.equal(parts.requiredFormat, 'jpeg');
+  assert.equal(parts.imageName, 'image.jpeg');
+  assert.equal(parts.sourceKey, 'image.jpeg');
+  assert.equal(parts.scaledKey, '300x400/jpeg/image.jpeg');
+});
 
-  it('parses format with path but no prefix', () => {
-    const testURI = '/images/300x400/jpeg/image.jpeg'
-    let urip = URIParser(testURI)
-    let parts = urip.getParts();
-    expect(parts.width).toEqual(300);
-    expect(parts.height).toEqual(400);
-    expect(parts.prefix).toBe("images")
-    expect(parts.requiredFormat).toEqual('jpeg');
-    expect(parts.imageName).toEqual('image.jpeg');
-    expect(parts.sourceKey).toEqual('images/image.jpeg');
-    expect(parts.scaledKey).toEqual('images/300x400/jpeg/image.jpeg');
-  });
+test('parses format with path but no prefix', () => {
+  const testURI = '/images/300x400/jpeg/image.jpeg'
+  let urip = URIParser(testURI)
+  let parts = urip.getParts();
+  assert.equal(parts.width, 300);
+  assert.equal(parts.height, 400);
+  assert.equal(parts.prefix, 'images');
+  assert.equal(parts.requiredFormat, 'jpeg');
+  assert.equal(parts.imageName, 'image.jpeg');
+  assert.equal(parts.sourceKey, 'images/image.jpeg');
+  assert.equal(parts.scaledKey, 'images/300x400/jpeg/image.jpeg');
 });
