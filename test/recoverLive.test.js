1d5c5eb8422b35113bb08b148fcf025f1c7568dd
Douglas Lovell <doug@wbreeze.com>
2022-12-16 17:33:44 -0300

    Provide test that exercises aws s3 api live against a bucket

---
+++ test/recoverLive.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+import { ImageRequestBuilder } from "../src/imageRequestBuilder.js";
+import { ImageHandler, processEvent } from "../src/recover.js";
+import { createMockCF } from "./createMockCF.js";
+
+/**
+This test invokes the S3 API to query and touch an image on S3.
+As such, this test is a "live" test, not a mock test.
+Use the test to reproduce the activity that occurs when the
+CloudFront server armed with the `recover` script fails to return
+a scaled version of an image that exists in the source image bucket.
+
+The environment from which you invoke the test will require the following:
+
+AWS_S3_RESIZE_REGION - region of the source and derived image buckets
+AWS_S3_RESIZE_SRC_NAME - address of the source image bucket
+AWS_S3_RESIZE_IMAGE_PATH - full path (key) of an image in the source image bucket
+
+Change `{ skip: 'live test touches S3' }` to
+`{ skip: false }` in order to run the test.
+Invoke as `node --test test/recoverLive.test.js`.
+**/
+
+test('touch image on S3 source bucket', () => {
+  const SRC_IMAGE = process.env.AWS_S3_RESIZE_IMAGE_PATH;
+  if (SRC_IMAGE === undefined) {
+    assert.fail('Configure environment variable, "AWS_S3_RESIZE_IMAGE_PATH"');
+  };
+  let mockCFNotFound = createMockCF();
+  let mockRequest = mockCFNotFound.Records[0].cf.request;
+  let mockResponse = mockCFNotFound.Records[0].cf.response;
+
+  var irb = ImageRequestBuilder(SRC_IMAGE, {'d':{'value':'600'}}, 'image/webp');
+  mockRequest.uri = irb.edgeRequest()
+  console.log("Request URI is %s", mockRequest.uri);
+
+  let handler = ImageHandler(mockRequest, mockResponse);
+  return handler.processResponse().then((response) => {
+    assert.equal(response.status, 302);
+    assert.equal(response.statusDescription, 'Found');
+    assert.ok(response.headers.location[0].value.includes(SRC_IMAGE));
+    return response;
+  });
+});
+
