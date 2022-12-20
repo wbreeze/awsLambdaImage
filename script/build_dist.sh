562bd8284c0682181531c106304421868aa727da
Douglas Lovell <doug@wbreeze.com>
2022-12-20 08:41:55 -0300

    Revise build script using parcel

--- script/build_dist.sh
+++ script/build_dist.sh
@@ -1,18 +1,10 @@
 if [[ -n ${AWS_S3_RESIZE_SRC_NAME} && -n ${AWS_S3_RESIZE_DST_NAME} && -n ${AWS_S3_RESIZE_REGION} ]]; then
   [[ -e dist ]] || mkdir dist
-  rm -rf node_modules/
-  npm install --only=prod
-  sed -e \
-    "s/process.env.AWS_S3_RESIZE_SRC_NAME/\"${AWS_S3_RESIZE_SRC_NAME}\"/g" \
-    resize.js \
-    | sed -e \
-    "s/process.env.AWS_S3_RESIZE_DST_NAME/\"${AWS_S3_RESIZE_DST_NAME}\"/g" \
-    | sed -e \
-    "s/process.env.AWS_S3_RESIZE_REGION/\"${AWS_S3_RESIZE_REGION}\"/g" \
-    >index.js
-  zip -r dist/resize.zip index.js node_modules
-  rm index.js
-  npm install
+  npx parcel build
+  cd dist
+  zip recover.zip recover.js
+  #rm index.js
+  cd ..
 else
   echo "Missing environment variable"
   exit 2
